2da07b9a37c6fcd4af544370b51734d797cef548
amalatp <amalahameedtp@gmail.com>
2025-02-10 23:24:14 +0530

    bottom nav bar

--- lib/features/bottom_nav_bar/presentation/screens/bottom_nav_bar.dart
+++ lib/features/bottom_nav_bar/presentation/screens/bottom_nav_bar.dart
@@ -37,11 +37,13 @@
 // }
 import 'package:flutter/material.dart';
 import 'package:grocery_shopping_app/configs/components/default_app_bar.dart';
+import 'package:grocery_shopping_app/configs/constants/app_svg.dart';
 import 'package:grocery_shopping_app/configs/extension/mediaquery_extension.dart';
 import 'package:grocery_shopping_app/configs/theme/app_colors.dart';
 import 'package:grocery_shopping_app/features/home/presentation/screens/home_screen.dart';
 import 'package:grocery_shopping_app/features/profile/presentation/profile_screen.dart';
 import 'package:grocery_shopping_app/features/wishlist/presentation/screens/wishlist_screen.dart';
+import 'package:svg_flutter/svg.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -70,31 +72,42 @@ class _BottomNavBarState extends State<BottomNavBar> {
     double height = context.mediaQueryHeight;
     return Scaffold(
       body: _widgetOptions[_selectedIndex], // Display selected screen
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildNavItem(Icons.home, "Home", 0),
-              _buildNavItem(Icons.favorite, "Wishlist", 1),
-              _buildNavItem(Icons.person, "Profile", 2),
-            ],
-          ),
+      bottomNavigationBar: Container(
+        height: height * 0.07,
+        margin: EdgeInsets.only(right: 20, left: 20, bottom: height * 0.015),
+        padding: const EdgeInsets.all(7),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                  color: Color.fromRGBO(116, 116, 116, 0.15),
+                  blurRadius: 25,
+                  spreadRadius: 0,
+                  offset: Offset(0, 4))
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildNavItem(AppSvg.home, "Home", 0),
+            _buildNavItem(AppSvg.wishlist, "Wishlist", 1),
+            _buildNavItem(AppSvg.profile, "Profile", 2),
+          ],
         ),
       ),
     );
   }
 
-  Widget _buildNavItem(IconData icon, String label, int index) {
+  Widget _buildNavItem(String icon, String label, int index) {
     bool isSelected = _selectedIndex == index;
     double height = context.mediaQueryHeight;
+    double width = context.mediaQueryWidth;
     return GestureDetector(
       onTap: () => _onItemTapped(index),
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 200),
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+        width: width * 0.25,
+        height: height * 0.07,
         decoration: isSelected
             ? BoxDecoration(
                 color: AppColors.primary,
@@ -102,9 +115,11 @@ class _BottomNavBarState extends State<BottomNavBar> {
               )
             : null,
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
+            SvgPicture.asset(
               icon,
+              height: height * 0.025,
               color: isSelected ? Colors.white : AppColors.focus,
             ),
             if (isSelected) ...[
--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -13,92 +13,101 @@ class HomeScreen extends StatelessWidget {
         body: SafeArea(
             child: Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: height * 0.03),
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'search',
-              suffixIcon: const Icon(Icons.search),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: height * 0.03),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Color.fromRGBO(116, 116, 116, 0.15),
+                        blurRadius: 25,
+                        spreadRadius: 0,
+                        offset: Offset(0, 4))
+                  ]),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'search',
+                  suffixIcon: const Icon(Icons.search),
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                ),
+                cursorColor: AppColors.focus,
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              contentPadding:
-                  const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
             ),
-            cursorColor: AppColors.focus,
-          ),
-          SizedBox(height: height * 0.03),
-          Text(
-            "Popular Products",
-            style: TextStyle(
-                fontSize: height * 0.018, fontWeight: FontWeight.w600),
-          ),
-          SizedBox(height: height * 0.02),
-          GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: .72,
-                crossAxisSpacing: width * 0.05,
-                mainAxisSpacing: height * 0.04,
-              ),
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: width * 0.46,
-                      decoration: BoxDecoration(
-                          color: AppColors.primary,
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          " Price ",
-                          style: TextStyle(
-                              decoration: TextDecoration.lineThrough,
-                              decorationColor: AppColors.focus,
-                              decorationThickness: 3,
-                              color: AppColors.focus,
-                              fontSize: height * 0.014,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        Text(
-                          " Price",
-                          style: TextStyle(
-                              color: AppColors.primary,
-                              fontSize: height * 0.016,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        const Spacer(),
-                        Text(
-                          "4.5",
-                          style: TextStyle(
-                            fontSize: height * 0.014,
+            SizedBox(height: height * 0.03),
+            Text(
+              "Popular Products",
+              style: TextStyle(
+                  fontSize: height * 0.018, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(height: height * 0.02),
+            GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: .72,
+                  crossAxisSpacing: width * 0.05,
+                  mainAxisSpacing: height * 0.04,
+                ),
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: width * 0.46,
+                        decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            " Price ",
+                            style: TextStyle(
+                                decoration: TextDecoration.lineThrough,
+                                decorationColor: AppColors.focus,
+                                decorationThickness: 3,
+                                color: AppColors.focus,
+                                fontSize: height * 0.014,
+                                fontWeight: FontWeight.w600),
                           ),
-                        )
-                      ],
-                    ),
-                    Text(
-                      "Product Name",
-                      style: TextStyle(
-                          fontSize: height * 0.016,
-                          fontWeight: FontWeight.w600),
-                    )
-                  ],
-                );
-              })
-        ],
+                          Text(
+                            " Price",
+                            style: TextStyle(
+                                color: AppColors.primary,
+                                fontSize: height * 0.016,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          const Spacer(),
+                          Text(
+                            "4.5",
+                            style: TextStyle(
+                              fontSize: height * 0.014,
+                            ),
+                          )
+                        ],
+                      ),
+                      Text(
+                        "Product Name",
+                        style: TextStyle(
+                            fontSize: height * 0.016,
+                            fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  );
+                })
+          ],
+        ),
       ),
     )));
   }
